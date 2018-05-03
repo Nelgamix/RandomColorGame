39bcfd84796bd5e222ec0148afd98d9aeb9c4c2d
Nelgamix <nelgamix@gmail.com>
2018-05-03 23:58:22 +0200

    premier test

--- app/src/main/java/com/skysurf/kryoxem/randomcolorgame/MenuActivity.kt
+++ app/src/main/java/com/skysurf/kryoxem/randomcolorgame/MenuActivity.kt
@@ -1,133 +1,126 @@
 package com.skysurf.kryoxem.randomcolorgame
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import kotlinx.android.synthetic.main.activity_menu.*
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.jaredrummler.android.colorpicker.ColorPickerDialog
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener
+import java.util.*
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-class MenuActivity : AppCompatActivity() {
-    private val mHideHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
+class MenuActivity : AppCompatActivity(), ColorPickerDialogListener {
+    private val TIMER: Long = 4000
 
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        fullscreen_content.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    }
-    private val mShowPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-        fullscreen_content_controls.visibility = View.VISIBLE
-    }
-    private var mVisible: Boolean = false
-    private val mHideRunnable = Runnable { hide() }
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val mDelayHideTouchListener = View.OnTouchListener { _, _ ->
-        if (AUTO_HIDE) {
-            delayedHide(AUTO_HIDE_DELAY_MILLIS)
-        }
-        false
-    }
+    private lateinit var mButtonNew : Button
+    private lateinit var mButtonOCP : Button
+    private lateinit var mButtonValidate : Button
+    private lateinit var mTitle : TextView
+    private lateinit var mBackground : View
+
+    private var isPlaying = false
+
+    private var colorPicked: Int = Color.BLACK
+    private var color: Int = Color.BLACK
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_menu)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        mVisible = true
+        mButtonNew = findViewById(R.id.activity_menu_new)
+        mButtonOCP = findViewById(R.id.activity_menu_open_colorpicker)
+        mButtonValidate = findViewById(R.id.activity_menu_validate)
+        mTitle = findViewById(R.id.activity_menu_title)
+        mBackground = findViewById(R.id.activity_menu_background)
 
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreen_content.setOnClickListener { toggle() }
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        dummy_button.setOnTouchListener(mDelayHideTouchListener)
+        update()
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100)
+    override fun onDialogDismissed(dialogId: Int) {
     }
 
-    private fun toggle() {
-        if (mVisible) {
-            hide()
+    override fun onColorSelected(dialogId: Int, color: Int) {
+        setBackgroundColor(color)
+        colorPicked = color
+    }
+
+    fun newGame(view: View) {
+        val randomColor = randomColor()
+        setBackgroundColor(randomColor)
+        color = randomColor
+
+        val handler = Handler()
+        handler.postDelayed({
+            beginGame()
+        }, TIMER)
+    }
+
+    fun validate(view: View) {
+        endGame()
+    }
+
+    fun openColorPicker(view: View) {
+        ColorPickerDialog.newBuilder()
+                .setDialogType(ColorPickerDialog.TYPE_CUSTOM)
+                .setAllowPresets(false)
+                .show(this)
+    }
+
+    private fun update() {
+        setBackgroundColor(Color.WHITE)
+        if (isPlaying) {
+            mButtonOCP.isEnabled = true
+            mButtonOCP.visibility = View.VISIBLE
+            mButtonValidate.isEnabled = true
+            mTitle.text = getString(R.string.menu_prompt)
         } else {
-            show()
+            mButtonOCP.visibility = View.GONE
+            mButtonValidate.isEnabled = false
+            mButtonOCP.isEnabled = false
+            mTitle.text = getString(R.string.menu_remember)
         }
     }
 
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-        fullscreen_content_controls.visibility = View.GONE
-        mVisible = false
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable)
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    private fun beginGame() {
+        isPlaying = true
+        update()
+        mBackground.setBackgroundColor(Color.WHITE)
     }
 
-    private fun show() {
-        // Show the system bar
-        fullscreen_content.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        mVisible = true
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable)
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    private fun endGame() {
+        isPlaying = false
+        update()
+        Toast.makeText(this, "Diff: ${calculateDiffColors(color, colorPicked)}", Toast.LENGTH_LONG).show()
     }
 
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private fun delayedHide(delayMillis: Int) {
-        mHideHandler.removeCallbacks(mHideRunnable)
-        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    private fun calculateDiffColors(color1: Int, color2: Int): Double {
+        val c1r = Color.red(color1)
+        val c1g = Color.green(color1)
+        val c1b = Color.blue(color1)
+
+        val c2r = Color.red(color2)
+        val c2g = Color.green(color2)
+        val c2b = Color.blue(color2)
+
+        val rmean = (c1r + c2r) / 2
+        val r = c1r - c2r
+        val g = c1g - c2g
+        val b = c1b - c2b
+        val weightR = 2 + rmean / 256
+        val weightG = 4.0
+        val weightB = 2 + (255 - rmean) / 256
+        return Math.sqrt(weightR * r.toDouble() * r.toDouble() + weightG * g.toDouble() * g.toDouble() + weightB * b.toDouble() * b.toDouble())
     }
 
-    companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private val AUTO_HIDE = true
+    private fun setBackgroundColor(color: Int) {
+        mBackground.setBackgroundColor(color)
+    }
 
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private val UI_ANIMATION_DELAY = 300
+    private fun randomColor(): Int {
+        val r = Random()
+        return Color.rgb(r.nextInt(256), r.nextInt(256), r.nextInt(256))
     }
 }
